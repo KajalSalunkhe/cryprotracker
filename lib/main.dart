a177090df72489b75ea7d414d700e568477434c5
KajalSalunkhe <kajalsalunkhe26@gmail.com>
2023-12-12 15:00:37 +0530

    crypto first commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:cryptotracker/constants/Themes.dart';
+import 'package:cryptotracker/pages/home_page.dart';
+import 'package:cryptotracker/providers/ad_provider.dart';
+import 'package:cryptotracker/providers/market_provider.dart';
+import 'package:cryptotracker/providers/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:provider/provider.dart';
+
+import 'services.dart/local_storage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+
+  String currentTheme = await LocalStorage.getTheme() ?? "light";
+
+  runApp(MyApp(
+    theme: currentTheme,
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  final String theme;
+
+  MyApp({required this.theme});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<MarketProvider>(
+          create: (context) => MarketProvider(),
+        ),
+        ChangeNotifierProvider<ThemeProvider>(
+          create: (context) => ThemeProvider(theme),
+        ),
+        ChangeNotifierProvider<AdProvider>(
+          create: (context) => AdProvider(),
+        ),
+      ],
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            themeMode: themeProvider.themeMode,
+            theme: lightTheme,
+            darkTheme: darkTheme,
+            home: HomePage(),
+          );
+        },
+      ),
+    );
+  }
+}
